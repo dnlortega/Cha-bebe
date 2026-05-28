b401b71efb66d68184ca46c174fe8ee9c8223c69
dnlortega <dnlortega@gmail.com>
2026-05-28 16:35:21 -0300

    fix: allow googleusercontent in CSP img-src for avatars

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,7 +12,7 @@ export function middleware(request: NextRequest) {
     "default-src 'self'",
     `script-src ${scriptSrc}`,
     "style-src 'self' 'unsafe-inline'",
-    "img-src 'self' data:",
+    "img-src 'self' data: https://*.googleusercontent.com",
     "font-src 'self'",
     "connect-src 'self'",
     "frame-src 'none'",
